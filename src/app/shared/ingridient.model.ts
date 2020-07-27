d15578753a9d2dc35b40b190bdad300cf4ba93a3
Pulkit2121 <rajpalpulkit21@gmail.com>
2020-07-28 00:58:47 +0530

    layout

---
+++ src/app/shared/ingridient.model.ts
@@ -0,0 +1,14 @@
+export class Ingridient{
+    // public name:string;
+    // public amount:number;
+
+
+    // Ingridient(name:string, amount:number){
+    //     this.name=name;
+    //     this.amount=amount;
+    // }
+
+    constructor(public name:string, public amount:number){
+        
+    }
+}
